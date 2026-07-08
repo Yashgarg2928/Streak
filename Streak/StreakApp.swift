c93dd00eb7e9ffd2c67a22b11c26b4741a96353c
Yashgarg2928 <y29garg@gmail.com.com>
2026-07-08 22:12:15 +0530

    Initial Commit

---
+++ Streak/StreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreakApp.swift
+//  Streak
+//
+//  Created by Madhvan on 08/07/26.
+//
+
+import SwiftUI
+
+@main
+struct StreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
